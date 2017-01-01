4bca479f441bf51c2873b14cadeee63d8f6dd985
karagiannis <lasse.l.karagiannis@gmail.com>
2017-01-01 07:27:14 +0100

    Multiple definitions of font8x8_basic.h

--- HW_C_engjob/HW_C_ingjob/software/C_eng_job4/obj/default/font8x8_basic.d
+++
@@ -1,4 +0,0 @@
-obj/default/font8x8_basic.o: font8x8_basic.c \
- ../C_eng_job4_bsp//HAL/inc/alt_types.h
-
-../C_eng_job4_bsp//HAL/inc/alt_types.h:
--- HW_C_engjob/HW_C_ingjob/software/C_eng_job4/obj/default/main.d
+++ HW_C_engjob/HW_C_ingjob/software/C_eng_job4/obj/default/main.d
@@ -2,8 +2,9 @@ obj/default/main.o: main.c ../C_eng_job4_bsp/system.h \
  ../C_eng_job4_bsp/linker.h ../C_eng_job4_bsp//HAL/inc/io.h \
  ../C_eng_job4_bsp//HAL/inc/alt_types.h \
  ../C_eng_job4_bsp//HAL/inc/alt_types.h \
- ../C_eng_job4_bsp//drivers/inc/BeMicro_VGA_IP_Driver.h font8x8_basic.h \
- vga_util.h sensor.h ../C_eng_job4_bsp//drivers/inc/altera_avalon_spi.h \
+ ../C_eng_job4_bsp//drivers/inc/BeMicro_VGA_IP_Driver.h vga_util.h \
+ font8x8_basic.h sensor.h \
+ ../C_eng_job4_bsp//drivers/inc/altera_avalon_spi.h \
  ../C_eng_job4_bsp//drivers/inc/altera_avalon_spi_regs.h \
  ../C_eng_job4_bsp//drivers/inc/altera_avalon_timer_regs.h
 
@@ -19,9 +20,9 @@ obj/default/main.o: main.c ../C_eng_job4_bsp/system.h \
 
 ../C_eng_job4_bsp//drivers/inc/BeMicro_VGA_IP_Driver.h:
 
-font8x8_basic.h:
-
 vga_util.h:
+
+font8x8_basic.h:
 
 sensor.h:
 
